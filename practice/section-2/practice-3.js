9f019f713f68cd585809cd66a4cd028997dfbd45
CAYDE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUCA6@OOCL.COM>
2019-07-08 17:24:04 +0800

    changed

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -4,20 +4,38 @@ function countSameElements(collection) {
   let result = [];
   let map = new Map();
   for(let i = 0; i < collection.length; i++){
+    if(collection[i].length !== 1){
+      continue;
+    }
     if(map.has(collection[i])){
       map.set(collection[i], map.get(collection[i]) + 1);
     }else{
       map.set(collection[i], 1);
     }
   }
-  console.log(Array.from(map));
-  map.forEach((value, key, map) => {
-    console.log(key);
-    if(key.length == 1){
-      result.push({key: key, count: value});
-    }else{
-      result.push({key: key.split('-')[0], count: parseInt(key.split('-')[1])})
+  collection.forEach((item) => {
+    if(item.indexOf('-') >= 0){
+      if(map.get(item.split('-')[0])){
+        map.set(item.split('-')[0], map.get(item.split('-')[0]) + parseInt(item.split('-')[1]));
+      }else{
+        map.set(item.split('-')[0], parseInt(item.split('-')[1]));
+      }
+    }else if(item.indexOf(':') >= 0){
+      if(map.get(item.split(':')[0])){
+        map.set(item.split(':')[0], map.get(item.split(':')[0]) + parseInt(item.split(':')[1]));
+      }else{
+        map.set(item.split(':')[0], parseInt(item.split(':')[1]));
+      }
+    }else if(item.indexOf('[') >= 0){
+      if(!map.get(item.split('-')[0])){
+        map.set(item.split('[')[0], map.get(item.split('[')[0]) + parseInt(item.substring(item.indexOf('[') + 1, item.indexOf(']'))));
+      }else{
+        map.set(item.split('[')[0], parseInt(item.substring(item.indexOf('[') + 1, item.indexOf(']'))));
+      }
     }
+  })
+  map.forEach((value, key, map) => {
+    result.push({name: key, summary: value});
   });
   return result;
 }
